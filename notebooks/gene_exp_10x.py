b6bca7b109456b1e45ee46c40483edaee9673059
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-06-18 15:47:01 -0400

    improving gene_exp_10x load data function

--- notebooks/gene_exp_10x.py
+++ notebooks/gene_exp_10x.py
@@ -19,18 +19,52 @@ def load_gene_exp_to_df(inst_path):
   lines = f.readlines()
   f.close()
 
-  genes = []
-  unique_id = 0
+  # # add unique id to all genes
+  # genes = []
+  # unique_id = 0
+  # for inst_line in lines:
+  #     inst_line = inst_line.strip().split()
+
+  #     if len(inst_line) > 1:
+  #       inst_gene = inst_line[1]
+  #     else:
+  #       inst_gene = inst_line[0]
+
+  #     genes.append(inst_gene + '_' + str(unique_id))
+  #     unique_id = unique_id + 1
+
+  # add unique id only to duplicate genes
+  ini_genes = []
   for inst_line in lines:
       inst_line = inst_line.strip().split()
-
       if len(inst_line) > 1:
         inst_gene = inst_line[1]
       else:
         inst_gene = inst_line[0]
+      ini_genes.append(inst_gene)
 
-      genes.append(inst_gene + '_' + str(unique_id))
-      unique_id = unique_id + 1
+  gene_name_count = pd.Series(ini_genes).value_counts()
+  duplicate_genes = gene_name_count[gene_name_count > 1].index.tolist()
+
+  dup_index = {}
+  genes = []
+  for inst_row in ini_genes:
+
+    # add index to non-unique genes
+    if inst_row in duplicate_genes:
+
+      # calc_non-unque index
+      if inst_row not in dup_index:
+        dup_index[inst_row] = 1
+      else:
+        dup_index[inst_row] = dup_index[inst_row] + 1
+
+      new_row = inst_row + '_' + str(dup_index[inst_row])
+
+    else:
+      new_row = inst_row
+
+    genes.append(new_row)
 
   # barcodes
   filename = inst_path + 'barcodes.tsv'
